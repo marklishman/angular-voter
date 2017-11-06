523cbcc385623f3abf55686b9fe2a351f28f4223
marklishman <mark.lishman@gmail.com>
2017-11-06 13:34:45 +0000

    Fixed JSLint errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { VoterComponent }   from './voter.component';
+import { VoterComponent } from './voter.component';
 
 @NgModule({
     imports:      [BrowserModule],
